7589ee5fd8bc96f0605599fa4fd3f04eb9d53a8c
sarasaad11 <55885702+sarasaad11@users.noreply.github.com>
2023-02-02 13:15:55 +0300

    update connect.php

--- connect.php
+++ connect.php
@@ -1,18 +1,48 @@
 <?php
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$pswrepeat = $_POST['pswrepeat'];
-//Database connection
-$conn = new mysqli('local','root','','signin');
-if($conn->connection_error){
-    die('Connection Field : '.$conn->connect_error);
-} else{
-    $stmt = $conn->prepare("insert into registrition(username, email, password, passrepeat) values(?, ?, ?, ?)");
-    $stmt->bind_param("ssss",$username, $email, $password, $passrepeat)
-    $stmt->execute();
-    echo "registration Successfully...";
-    $stmt->close();
-    $conn_>close();
+
+$servername="localhost";
+$dbname="signin";
+$username="root";
+$password="";
+
+if(isset($_POST['submit'])) {
+
+
+try {
+	
+	$conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+ // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$passrepeat = $_POST['passrepeat'];
+
+ $stmt = $conn->prepare("INSERT INTO registration ( username, email, password, passrepeat)  
+   VALUES (:username, :email, :password, :passrepeat )");
+  $stmt->bindParam(':username', $username);
+  $stmt->bindParam(':email', $email);
+  $stmt->bindParam(':password', $password);
+  $stmt->bindParam(':passrepeat', $passrepeat);
+
+   if($stmt->execute())
+      {
+	   echo "New records created successfully";
+	   
+	   header('location: login.html');
+	   }
+	   
+	   else{
+      echo "error";
+     }
+
+
+   
+   }
+   catch(PDOException $e)
+   {
+   echo "Error: " . $e->getMessage();
+    }
+   $conn = null;
 }
 ?>
